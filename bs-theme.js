753de2671f8d867de98e4b6541dfe95b615535fe
MarinCodeDev <marinlebailly@outlook.fr>
2024-06-19 16:51:32 +0200

    Mode light/dark...

---
+++ bs-theme.js
@@ -0,0 +1,87 @@
+/*!
+ * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
+ * Copyright 2011-2024 The Bootstrap Authors
+ * Licensed under the Creative Commons Attribution 3.0 Unported License.
+ */
+(() => {
+    'use strict'
+
+//     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+//     // var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+//     console.log (tooltipTriggerList)
+    // var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    //     return new bootstrap.Tooltip(tooltipTriggerEl)
+    // })
+    
+    const getStoredTheme = () => localStorage.getItem('theme')
+    const setStoredTheme = theme => localStorage.setItem('theme', theme)
+
+    const getPreferredTheme = () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme) {
+            return storedTheme
+        }
+
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+    }
+
+    const setTheme = theme => {
+        if (theme === 'auto') {
+            document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+        } else {
+            document.documentElement.setAttribute('data-bs-theme', theme)
+        }
+    }
+
+    setTheme(getPreferredTheme())
+
+    const showActiveTheme = (theme, focus = false) => {
+        const themeSwitcher = document.querySelector('#bd-theme')
+
+        if (!themeSwitcher) {
+            return
+        }
+
+        const themeSwitcherText = document.querySelector('#bd-theme-text')
+        const activeThemeIcon = document.querySelector('.theme-icon-active i')
+        const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+        const spanOfActiveBtn = btnToActive.querySelector('span i').getAttribute('class')
+
+        document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+            element.classList.remove('active')
+            element.setAttribute('aria-pressed', 'false')
+        })
+
+        btnToActive.classList.add('active')
+        btnToActive.setAttribute('aria-pressed', 'true')
+        activeThemeIcon.setAttribute('class', spanOfActiveBtn)
+        const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
+        themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
+
+        if (focus) {
+            themeSwitcher.focus()
+        }
+    }
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+        const storedTheme = getStoredTheme()
+        if (storedTheme !== 'light' && storedTheme !== 'dark') {
+            setTheme(getPreferredTheme())
+        }
+    })
+
+    window.addEventListener('DOMContentLoaded', () => {
+        showActiveTheme(getPreferredTheme())
+
+        document.querySelectorAll('[data-bs-theme-value]')
+            .forEach(toggle => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value')
+                    setStoredTheme(theme)
+                    setTheme(theme)
+                    showActiveTheme(theme, true)
+                })
+            })
+    })
+})()
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-import {createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+import { createApp, ref } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 const App = {
     data() {
         const storageData = this.load();
-        
+
         console.log("storageData", storageData);
         return {
             page: "notes",
-            notes: storageData.notes,
+            histories: [],
 
+            notes: storageData.notes,
             trashes: storageData.trashes,
 
             newNote: ""
@@ -27,6 +28,7 @@ const App = {
 
         addNote() {
             if (this.newNote != "") {
+                this.keepHistory()
                 this.notes.push(
                     {
                         message: this.newNote,
@@ -38,27 +40,36 @@ const App = {
             }
         },
 
-        startEdit(note){
+        checkRefresh() {
+            this.keepHistory();
+            this.save();
+        },
+
+        startEdit(note) {
+            this.keepHistory();
             note.edition = true
         },
 
-        endEdit(note){
+        endEdit(note) {
             note.edition = false
         },
 
         removeNote(index) {
+            this.keepHistory();
             this.trashes.push(this.notes[index]);
             this.notes.splice(index, 1);
             this.save()
         },
 
         removeAllNote() {
+            this.keepHistory();
             this.trashes = this.trashes.concat(this.notes);
             this.notes = [];
             this.save()
         },
 
         removeDoneNote() {
+            this.keepHistory();
             var noteDone = this.notes.filter((note) => note.done == true);
             this.notes = this.notes.filter((note) => note.done == false);
             this.trashes = this.trashes.concat(noteDone);
@@ -66,11 +77,13 @@ const App = {
         },
 
         removeTrash(index) {
+            this.keepHistory();
             this.trashes.splice(index, 1);
             this.save()
         },
 
         removeAllTrash() {
+            this.keepHistory();
             this.trashes = [];
             this.save()
         },
@@ -79,18 +92,41 @@ const App = {
             window.localStorage.setItem("trashes", JSON.stringify(this.trashes));
             window.localStorage.setItem("notes", JSON.stringify(this.notes));
         },
-        
+
         loadItem(itemId) {
             let values = window.localStorage.getItem(itemId);
             return values ? JSON.parse(values) : [];
         },
-            
+
         load() {
             return {
                 trashes: this.loadItem("trashes"),
                 notes: this.loadItem("notes"),
             };
         },
+
+        keepHistory() {
+            this.histories.splice(0, 0, {
+                trashesJson: JSON.stringify(this.trashes),
+                notesJson: JSON.stringify(this.notes)
+            });
+        },
+
+        restoreHistory() {
+            if (!this.hasHistory()) {
+                return;
+            }
+
+            //            this.histories[0];
+            var history = (this.histories.splice(0, 1))[0];
+
+            this.trashes = JSON.parse(history.trashesJson);
+            this.notes = JSON.parse(history.notesJson);
+        },
+
+        hasHistory() {
+            return Boolean(this.histories.length);
+        }
 
     }
 }
